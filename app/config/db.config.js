743a9adcce8ae3c0b31d8ee370335b20ad9e8623
Patrick Ceriale <patceriale@gmail.com>
2021-12-13 15:40:12 -0800

    configuring heroku db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: 'password',
-  DB: 'funktionjunction_db',
+  HOST: 'us-cdbr-east-05.cleardb.net',
+  USER: 'be7509ee88be0a',
+  PASSWORD: '444001ba',
+  DB: 'heroku_d735562e7808766',
   dialect: 'mysql',
   pool: {
     max: 5,
@@ -10,4 +10,16 @@ module.exports = {
     acquire: 30000,
     idle: 10000,
   },
+  // Development config:
+  // HOST: 'localhost',
+  // USER: 'root',
+  // PASSWORD: 'password',
+  // DB: 'funktionjunction_db',
+  // dialect: 'mysql',
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000,
+  // },
 };
